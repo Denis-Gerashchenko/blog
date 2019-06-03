71ee16a6c8f7c1539834515a2fd3835f18b55958
Denis <denis.gerashchenko.qa@gmail.com>
2019-06-04 02:00:58 +0300

    Расфасовка индекса

--- posts/models.py
+++ posts/models.py
@@ -27,6 +27,7 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
+    main = models.BooleanField()
 
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from posts.models import Post
 
 def index(request):
-    return render(request, 'index.html', {})
+    queryset = Post.objects.filter(featured=True).order_by('-timestamp')[0:13]
+    row1 = queryset[0:3]
+    row2 = queryset[3:6]
+    row3_1 = queryset[6]
+    row3_2 = queryset[7]
+    row4 = queryset[8:11]
+    row5_1 = queryset[11]
+    row5_2 = queryset[12]
+    context = {
+        'row1' : row1,
+        'row2' : row2,
+        'row3_1' : row3_1,
+        'row3_2' : row3_2,
+        'row4' : row4,
+        'row5_1' : row5_1,
+        'row5_2' : row5_2
+    }
+    return render(request, 'index.html', context)
 
 def single(request):
     return render(request, 'single.html', {})
