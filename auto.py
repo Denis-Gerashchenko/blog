6b5b36a1376ceabd153cbabfd6076ef4364d970d
Denis <denis.gerashchenko@testmatick.com>
2019-06-17 13:02:34 +0300

    add auto.py

--- auto.py
+++ auto.py
@@ -18,14 +18,14 @@ message = args.Message
 password = args.Password
 login = 'Denis-Gerashchenko'
 
-# encoded_login = bytes(login, 'utf-8')
-# encoded_password = bytes(password, 'utf-8')
+encoded_login = bytes(login, 'utf-8')
+encoded_password = bytes(password, 'utf-8')
 
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', f'{message}'])
 proc = subprocess.Popen(['git', 'push', '-u', 'origin', 'master'], stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-proc.stdin.write(login)
-proc.stdin.write(password)
+proc.stdin.write(encoded_login)
+proc.stdin.write(encoded_password)
 
 
 
