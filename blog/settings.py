b97b56fda996641495fab8e1f732666dcd186fc5
Denis-Gerashchenko <denis.gerashchenko.qa@gmail.com>
2019-08-01 19:49:02 +0300

    added staticfiles finders

--- blog/settings.py
+++ blog/settings.py
@@ -172,13 +172,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
 STATICFILES_FINDERS = (
 'django.contrib.staticfiles.finders.FileSystemFinder',
 'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 )
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
