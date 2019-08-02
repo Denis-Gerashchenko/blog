805689ed712bbf7ab7940b0ba5524fb70830cc27
Denis <denis.gerashchenko.qa@gmail.com>
2019-08-02 05:46:23 +0300

    Crying in my skin fix

--- blog/settings.py
+++ blog/settings.py
@@ -176,6 +176,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env/'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
