329724180c67485ac5c066ec20961e087a3aadca
Denis <denis.gerashchenko.qa@gmail.com>
2019-08-02 05:56:31 +0300

    Crying in my skin fix #2

--- blog/settings.py
+++ blog/settings.py
@@ -173,8 +173,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/static_root/'
+MEDIA_URL = '/media_root/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env/'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
