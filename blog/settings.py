0e77b74dc1306585bafd33dbfc6f6bc591e5f11b
Denis-Gerashchenko <denis.gerashchenko.qa@gmail.com>
2019-08-04 16:53:49 +0300

    Add email backend

--- blog/settings.py
+++ blog/settings.py
@@ -119,6 +119,7 @@ DATABASES = {
                 'PORT': '',
             }
         }
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
