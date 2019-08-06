e4ce56e9b55084e19b71bdf6568868f4845303a3
Denis-Gerashchenko <46624843+Denis-Gerashchenko@users.noreply.github.com>
2019-08-06 18:11:31 +0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -173,8 +173,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'neonblog.sup@gmail.com'
-EMAIL_HOST_PASSWORD = 'neonblog1100'
+EMAIL_HOST_USER = 'ytgfc'
+EMAIL_HOST_PASSWORD = '091hjk'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
